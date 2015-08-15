5f289d47e134b6de093371bbb4fdcae1204a8759
Antonio Heard <heardantonio@gmail.com>
2015-08-14 17:44:41 -0700

    Adds redirects to show_error_output calls in add_friend.php

--- public_html/add_friend.php
+++ public_html/add_friend.php
@@ -3,13 +3,13 @@
 <?php require_once("../includes/functions.php"); ?>
 <?php
 	if (!isset($_SESSION["user_id"])) {
-		set_error_output("Please log in to add friend. ");
+		set_error_output("Please log in to add friend. ", "log_in.php");
 	}else if (!isset($_GET["user"])) {
-		set_error_output("Invalid friend. ");
+		set_error_output("Invalid friend. ", "index.php");
 	}else if ($_SESSION["user_id"] == $_GET["user"]) {
-		set_error_output("Cannot add yourself to your own friends list. ");
+		set_error_output("Cannot add yourself to your own friends list. ", "index.php");
 	}else if (already_friend($_SESSION["user_id"], $_GET["user"])) {
-		set_error_output("That user is already on your friends list. ");
+		set_error_output("That user is already on your friends list. ",  "user.php?user=" . urlencode($_GET["user"]));
 	}
 
 	add_friend($_SESSION["user_id"], $_GET["user"]);
