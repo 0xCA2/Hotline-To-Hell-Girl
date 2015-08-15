7c479a7ddc6dfc18aa61ba41de76fca0d599dded
Antonio Heard <heardantonio@gmail.com>
2015-08-15 06:56:31 -0700

    Cleans up public_html/delete_favorites.php by removing large old comment.

--- public_html/delete_favorites.php
+++ public_html/delete_favorites.php
@@ -2,33 +2,13 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 <?php
-/*
-	if (!isset($_SESSION["user_id"])) {
-		set_error_output("Please log in to delete favorite. ");
-	}else if (!isset($_GET["user"])) {
-		set_error_output("Favorite deletion failed. User not selected.");
-	}else if ($_SESSION["user_id"] != $_GET["user"]) {
-		set_error_output("Favorite deletion failed. ");
-	}else if (!isset($_GET["episode"])) {
-		set_error_output("Favorite deletion failed. Episode not selected. ");
-	}else if (!check_existance_by_id("episodes", "EpID", $_GET["episode"])) {
-		set_error_output("Episode does not exist." . check_existance_by_id("episodes", "EpID", $_GET["episode"]));
-	}else if (!check_existance_by_id("users", "id", $_GET["user"])) {
-		set_error_output("User does not exist. ");
-	}else if (!already_favorite($_GET["user"], $_GET["episode"])){
-		set_error_output("You never favorited that");
-	}
-	
-	if (!delete_favorite($_GET["user"], $_GET["episode"])){
-		set_error_output("Favorite deletion failed. Database error.");
-	}
-*/
+
 	if (isset($_POST["submit"])) {
 		$user_id = mysqli_real_escape_string($connection, $_POST["user_id"]);
 		$episode_id_list = mysqli_real_escape_string($connection, $_POST["episode_id_list"]);
-		
+
 		$episode_id_array = explode(" ", $episode_id_list);
-		
+
 		if (check_existance_by_id("users", "id", $user_id)) {
 			foreach($episode_id_array as $id) {
 				if (check_existance_by_id("episodes", "EpID", $id)){
@@ -36,7 +16,7 @@
 				}
 			}
 		}
-		
+
 	}
-	
+
 ?>
